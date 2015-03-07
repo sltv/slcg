c7344fca926c55200ab71c83abc53572f5049149
Phil Baldwin <phil@sheffieldlive.org>
2015-03-07 19:28:28 +0000

    Making connector

--- lib/connector.rb
+++ lib/connector.rb
@@ -1,4 +1,19 @@
-module Connector
-  def play
+require "socket"
+
+class Connector
+  attr_reader :opts
+  def initialize(opts={})
+    @opts = defaults.merge(opts)
+    @socket = TCPSocket::open(@opts[:host_name], @opts[:port])
+  end
+
+  def defaults
+    @defaults = {
+      host_name: '127.0.0.1',
+      port: 80
+    }
+  end
+
+  def connect(data)
   end
 end
